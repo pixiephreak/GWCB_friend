0136e1a5d04ac7c3fe745590f58d96e478bd3ebb
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2017-05-17 10:13:24 -0400

    wire get

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,7 @@
 var friends = require('../data/friends');
 
 module.exports = function(app){
-
+  //removed routes as body-parser wasn't running in here
 };
 
 //set content type? application/json ?
--- server.js
+++ server.js
@@ -16,9 +16,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+//api routes
+//require friends data for use in get
+var friends = require('./app/data/friends');
+
+app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
 app.post("/api/friends", function(req,res){
-  console.log(req);
-})
+  res.json(req.body);
+  friends.push(req.body);
+});
+
+
 
 // Initiate the listener.
 app.listen(port);
