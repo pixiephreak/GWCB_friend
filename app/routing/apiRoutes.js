62de37a49fe97566a09c654b7895764c2b55c160
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2017-06-06 16:51:08 -0400

    add proximity radio and in route

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,31 +17,36 @@ module.exports = function(app) {
     // Here we take the result of the user"s survey POST and parse it.
     var userData = req.body;
     var userScores = userData.scores;
+    var proximity = userData.proximity;
     var totalDifference = 0;
-    for (var i = 0; i < friends.length; i++) {
+    console.log(proximity);
 
-      console.log(friends[i].name);
-      totalDifference = 0;
+    if(proximity === 'similar'){
+      console.log('executing');
+      for (var i = 0; i < friends.length; i++) {
+        console.log(friends[i].name, friends[i].proximity);
+        totalDifference = 0;
 
-      // We then loop through all the scores of each friend
-      for (var j = 0; j < friends[i].scores[j]; j++) {
+        // We then loop through all the scores of each friend
+        for (var j = 0; j < friends[i].scores[j]; j++) {
 
-        // We calculate the difference between the scores and sum them into the totalDifference
-        totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+          // We calculate the difference between the scores and sum them into the totalDifference
+          totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
 
-        // If the sum of differences is less then the differences of the current "best match"
-        if (totalDifference <= bestMatch.friendDifference) {
+          // If the sum of differences is less then the differences of the current "best match"
+          if (totalDifference <= bestMatch.friendDifference) {
 
-          // Reset the bestMatch to be the new friend.
-          bestMatch.name = friends[i].name;
-          bestMatch.photo = friends[i].photo;
-          bestMatch.friendDifference = totalDifference;
+            // Reset the bestMatch to be the new friend.
+            bestMatch.name = friends[i].name;
+            bestMatch.photo = friends[i].photo;
+            bestMatch.friendDifference = totalDifference;
+          }
         }
       }
+      friends.push(userData);
+      res.json(bestMatch);
     }
 
-    friends.push(userData);
-    res.json(bestMatch);
   });
 
   app.get("/api/friends", function(req, res) {
