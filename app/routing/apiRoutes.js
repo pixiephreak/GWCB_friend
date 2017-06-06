cf4ce29fc1f33e3752ad92ec2a0bfddd2f59d8b7
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2017-06-06 10:54:50 -0400

    adding match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,9 +4,22 @@ var questions = require('../public/js/questions')
 module.exports = function(app){
 
     app.post("/api/friends", function(req, res) {
-      // console.log(req.body);
-      var newFriend = req.body;
-      friends.push(newFriend);
+      console.log('posting');
+      console.log(req.body);
+      // We will use this object to hold the "best match". We will constantly update it as we
+      // loop through all of the options
+      var bestMatch = {
+        name: "",
+        photo: "",
+        friendDifference: 1000
+      };
+
+      // Here we take the result of the user"s survey POST and parse it.
+      var userData = req.body;
+      var userScores = userData.scores;
+
+      // var newFriend = req.body;
+      // friends.push(newFriend);
       res.send(req.body);
     });
 
