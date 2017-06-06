ab38e3b47937daba1ed33f8fb3b391c152f657fa
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2017-06-06 15:58:45 -0400

    fisih matching logic and display in results div

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,29 +1,50 @@
 var friends = require('../data/friends');
 var questions = require('../public/js/questions')
 
-module.exports = function(app){
+module.exports = function(app) {
 
-    app.post("/api/friends", function(req, res) {
-      console.log('posting');
-      console.log(req.body);
-      // We will use this object to hold the "best match". We will constantly update it as we
-      // loop through all of the options
-      var bestMatch = {
-        name: "",
-        photo: "",
-        friendDifference: 1000
-      };
+  app.post("/api/friends", function(req, res) {
+    console.log('posting');
+    console.log(req.body);
+    // We will use this object to hold the "best match". We will constantly update it as we
+    // loop through all of the options
+    var bestMatch = {
+      name: "xxx",
+      photo: "xxx",
+      friendDifference: 1000
+    };
 
-      // Here we take the result of the user"s survey POST and parse it.
-      var userData = req.body;
-      var userScores = userData.scores;
+    // Here we take the result of the user"s survey POST and parse it.
+    var userData = req.body;
+    var userScores = userData.scores;
+    var totalDifference = 0;
+    for (var i = 0; i < friends.length; i++) {
 
-      // var newFriend = req.body;
-      // friends.push(newFriend);
-      res.send(req.body);
-    });
+      console.log(friends[i].name);
+      totalDifference = 0;
 
-    app.get("/api/friends", function(req, res) {
+      // We then loop through all the scores of each friend
+      for (var j = 0; j < friends[i].scores[j]; j++) {
+
+        // We calculate the difference between the scores and sum them into the totalDifference
+        totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+
+        // If the sum of differences is less then the differences of the current "best match"
+        if (totalDifference <= bestMatch.friendDifference) {
+
+          // Reset the bestMatch to be the new friend.
+          bestMatch.name = friends[i].name;
+          bestMatch.photo = friends[i].photo;
+          bestMatch.friendDifference = totalDifference;
+        }
+      }
+    }
+
+    friends.push(userData);
+    res.json(bestMatch);
+  });
+
+  app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
 
